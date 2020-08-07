40b52abd56ab10d5081cf71ee4946a9bc14ae501
krtsato <ev60596@meiji.ac.jp>
2020-08-08 01:57:32 +0900

    fix: type error in tags map

--- src/components/tags.tsx
+++ src/components/tags.tsx
@@ -1,11 +1,14 @@
 import React from "react"
 import Link from "next/link"
 
-export default (tags) => (
-  tags.map(tag => (
-    <Link href={`/tags/[tagName]`} as={`/tags/${tag}`} passHref prefetch={false}>
-      <a>{tag}</a>
-    </Link>
-    )
+export default ({tags}) => {
+  const tagArr = tags.split(",")
+
+  return (
+    tagArr.map(tag => (
+      <Link href={`/tags/[tagName]`} as={`/tags/${tag}`} passHref prefetch={false}>
+        <a>{tag}</a>
+      </Link>
+    ))
   )
-)
+}
